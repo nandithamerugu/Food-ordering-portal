c8bf2c17f03dd42e19ee9cbc5cd4498b3d3ffa1e
nandithamerugu <82519236+nandithamerugu@users.noreply.github.com>
2023-09-04 14:27:17 +0530

    Create Food.js

---
+++ backend/models/Food.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const FoodSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    rating:{
+        type:Number,
+        default:0,
+        // required: true
+    },
+   email:{
+        type:String,
+        required : true
+    },
+    CanteenName:{
+        type:String,
+        // required : true
+    },
+    ManagerName:{
+        type:String,
+        // required : true
+    },
+    ManagerContact:{
+        type:String,
+        // required : true
+    },
+    opening: {
+        type: String,
+        required: false
+    },
+    closing: {
+        type: String,
+        required: false
+    },
+
+
+    Address: {
+        type: String,
+        required: false
+    },
+    
+    add_ons:{
+        type:String,
+    },
+    FoodDiscription:{
+        type:String,
+        required:false
+    },
+    Quantity:{
+        type:Number,
+        default:1,
+        required:false
+    },
+    date:{
+		type: String,
+		required: false
+	}   
+
+});
+
+module.exports = Food = mongoose.model("Food", FoodSchema);
